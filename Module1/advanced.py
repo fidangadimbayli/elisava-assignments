73508929d23f7ed50d8a48f3fd6d9aa47515d104
fidangadimbayli <160627087+fidangadimbayli@users.noreply.github.com>
2024-11-07 12:13:23 +0100

    New assignments

---
+++ Module1/advanced.py
@@ -0,0 +1,27 @@
+# Advance - Excersize1 (Breaking down coins)
+
+
+
+
+
+
+
+
+
+
+
+# Advance - Excersize2 (Email Validation)
+# def user_input(email):
+#     if "@" in email and email.endswith(".com"):
+#         print("User input is valid")
+#     else:
+#         print("User input is invalid")
+# user_input("baxisli.fidan@gmail.com")
+# user_input("test.co")
+
+def and_or(a, b):
+    print(f'a OR b is {a | b}')
+    print(f'a AND b is {a & b}')
+
+and_or(True, False)
+
---
+++ Module1/middle.py
@@ -0,0 +1,30 @@
+# Middle - Excercise1 (Simple Interest Calculator)
+# def user_inputs(principal,rate,time):
+#     simple_interest_rate = principal * rate/100 * time
+#     print(simple_interest_rate)
+
+# user_inputs(20000,5,5)
+
+#  Middle - Excercise2 (BMI Calculator)
+# def user_inputs(weight,height):
+#     body_mass_index = (weight / (height * height))
+#     print(body_mass_index)
+
+# user_inputs(55,1.58)
+
+# Middle - Excercise3 (Value Swap)
+# def swap_values(a, b):
+#     a = a + b
+#     b = a - b
+#     a = a - b
+
+#     print(f'A is {a} B is {b}')
+
+# swap_values(5, 7)
+
+# Middle - Excercise4 (Character Count)
+# txt = "I love reading books"
+# character = "e"
+# result = txt.count(character)
+
+# print(result)
